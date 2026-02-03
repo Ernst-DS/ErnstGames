2544d2078b5246a51acfb6a60f4a4894109ac313
ernstpatat <desutterernst@gmail.com>
2026-02-03 03:05:42 +0100

    Render window

---
+++ Main.py
@@ -0,0 +1,17 @@
+﻿import pygame
+
+pygame.init()
+color = (255,255,255)
+position = (0,0)
+CANVAS = pygame.display.set_mode((1000,1000))
+pygame.display.set_caption("Ernst Games")
+running = True
+
+while running:
+    CANVAS.fill("white")
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
